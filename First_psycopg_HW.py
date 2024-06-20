2bcfa14f974fcf7389434b45f615a2a941d9cbc0
Sjx0ck <tim.dimytch@gmail.com>
2024-06-20 16:22:00 -0700

    changes

--- First_psycopg_HW.py
+++ First_psycopg_HW.py
@@ -6,7 +6,7 @@ def delete_table(cursor):
         DROP TABLE IF EXISTS phone;
         DROP TABLE IF EXISTS client;
     """)
-
+#A function that creates a database structure (tables).
 def tables(cursor):
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS client(
@@ -25,14 +25,7 @@ def tables(cursor):
         );
     """)
 
-def add_phone(cursor, client_id, phone):
-    cursor.execute("""
-        INSERT INTO phone (client_id, client_phone) VALUES (%s, %s)
-        RETURNING phone_id;
-    """, (client_id, phone))
-    phone_id = cursor.fetchone()[0]
-    return phone_id
-
+#A function that allows you to add a new client.
 def add_client(cursor, client_name, client_surname, client_email):
     cursor.execute("""
         
@@ -44,47 +37,60 @@ def add_client(cursor, client_name, client_surname, client_email):
     client_id = cursor.fetchone()[0]
     return client_id
 
+#A feature that allows you to add a phone for an existing customer.
+def add_phone(cursor, client_id, phone):
+    cursor.execute("""
+        INSERT INTO phone (client_id, client_phone) VALUES (%s, %s)
+        RETURNING phone_id;
+    """, (client_id, phone))
+    phone_id = cursor.fetchone()[0]
+    return phone_id
+
+#A function that allows you to change customer data.
 def update_client(cursor, client_id):
-    question = input("Выберите из списка и напишите точное название что вы хотите поменять в своих данных : client_name , client_surname , client_email, client_phone ")
-    question_2 = input("Напишите нужные вам данные: ")
+    question_1 = input("Select from the list and write the exact name that you want to change in your data : client_name , client_surname , client_email, client_phone ")
+    question_2 = input("Write the data you need: ")
 
-    if question in ['client_name', 'client_surname', 'client_email']:
+    if question_1 in ['client_name', 'client_surname', 'client_email']:
         cursor.execute(f"""
-            UPDATE client SET {question} = %s WHERE client_id = %s;
+            UPDATE client SET {question_1} = %s WHERE client_id = %s;
         """, (question_2, client_id))
+        return f"{question_1} has been succesfully updated"
 
-    elif question == 'client_phone':
+    elif question_1 == 'client_phone':
         cursor.execute("""
             UPDATE phone SET client_phone = %s WHERE client_id = %s;
         """, (question_2, client_id))
+        return "Client's phone number has been succesfully updated"
 
     else:
-        print("Некорректное название столбца")
+        return"Incorrect input!"
 
-def delete_phone(cursor,client_id, phone_id):
-    cur.execute("""
+#A feature that allows you to delete a phone for an existing customer.
+def delete_phone(cursor, client_id, phone_id):
+    cursor.execute("""
 
     DELETE FROM phone 
     WHERE phone_id = %s and client_id = %s
                     
     """, (phone_id, client_id,))
-    pass
- 
+    return "Client's phone number has been succesfully deleted"
+
+#A function that allows you to delete an existing client. 
 def delete_client(cursor,client_id):
-    cur.execute("""
+    cursor.execute("""
 
     DELETE FROM client 
     WHERE client_id = %s
                     
     """, (client_id,))
-    pass
-        
-        #Лучше бы тут наверное сделать через цикл for чтобы если бы было известно несколько своих данных было бы более точнее определить пользователя,
-        # но в задании просят "или" и у меня есть еще недоделанные работы. 
+    return "Client has been succesfully deleted"
+
+#A function that allows you to find a client by his data: first name, last name, email or phone number.
 def find_client(cursor):
 
-    question1 = input("Выберите из списка и напишите точное название что вам известно в своих данных в своих данных (что то одно) : client_name , client_surname , client_email, client_phone ")
-    question2 = input("Напишите информацию которая вам известна:   ")
+    question1 = input("Select from the list and write the exact name that you know in your data in your data (one thing) : client_name , client_surname , client_email, client_phone ")
+    question2 = input("Write down the data that you know:   ")
     
     if question1 in ["client_name" , "client_surname" , "client_email"]:
 
@@ -98,12 +104,12 @@ def find_client(cursor):
                 WHERE p.client_phone = %s;
                 """,(question2,))
     else:
-         print("Неверно назван столбец")
+         print("Incorrect input!")
          return None
 
 
     client_data = cursor.fetchall()
-    return client_data
+    return f"Your info: {client_data}"
         
 
 with psycopg2.connect(database="First_psycopg2", user="postgres", password="Tim597707") as conn:
@@ -112,18 +118,18 @@ with psycopg2.connect(database="First_psycopg2", user="postgres", password="Tim5
         delete_table(cur)
         tables(cur)
 
-        # Добавление нового клиента и телефона
+        # Adds new client and phone
         client_id = add_client(cur, 'John', 'Doe', 'john.doe@example.com')
         phone_id = add_phone(cur, client_id, '123-456-7890')
         
-        # Обновление данных клиента
+        # Update client's data
         update_client(cur, client_id)
         
-        # Поиск клиента
+        # Find a client
         clients = find_client(cur)
         print(clients)
         
-        # Удаление телефона и клиента
+        # delete phone and a client
         delete_phone(cur, client_id, phone_id)
         delete_client(cur, client_id)
 
